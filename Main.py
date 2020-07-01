a88a98629c394af50ac956388644851eb90287ae
Nabil <nkoney14@outlook.com>
2020-07-01 15:52:41 -0400

    Iterators

--- Main.py
+++ Main.py
@@ -1,16 +1,26 @@
-# NUMBERS
+# WHILE-LOOPS
 #
-# RANDOM NUMBERS
-# import random
-# random.randint(min_val, max_val)
+# ! using break statements !
+# ! using enumerate(list_name) returns tuples of indexes and list items in pairs!
 #
-# POWERS
-# num_1 ** num_2 (num_1 to the power of num_2)
+# EXAMPLE:
+# from random import randint
+# truth_condition = True
+# i = 0
+# list = [randint(1,100) for num in range(1000)]
+# while truth_condition:
+#     if list[i] == 25:
+#         print('25 found at index', i)
+#     i += 1
+#     if i >= len(list):
+#         truth_condition = False
 #
-# CASTING
-# type(val) e.g. str(2)
+# ZIP GENERATOR:
 #
-# USER INPUT
-# input('prompt_string') e.g. input('Enter first name: ')
-# can set input to variable; will always be type string
-#
+# returned as an object swo must cast to list to do stuff with it
+# combines lists into list of tuple pairs based on indexes of items in original lists
+# EXAMPLE:
+# first_names = ['spongebob', 'squidward', 'patrick']
+# last_names = ['squarepants', 'tentacles', 'star']
+# names = list(zip(first_names, last_names))
+# print(names)
